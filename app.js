d4fe6637d562a1896704ed17e5df521420ca3c7c
gabriel <nune.gabriel.gn@gmail.com>
2022-09-16 11:55:26 +0000

    mobile-menu

---
+++ app.js
@@ -0,0 +1,21 @@
+const menuBars = document.getElementById('hamburger');
+const curtain = document.getElementById('curtain');
+const nav1 = document.getElementById('nav1');
+const nav2 = document.getElementById('nav2');
+const nav3 = document.getElementById('nav3');
+const navList = [nav1, nav2, nav3];
+
+function HamburgerMenu() {
+  curtain.classList.toggle('CurTain');
+  menuBars.classList.toggle('change');
+  if (curtain.classList.contains('CurTain')) {
+    curtain.classList.replace('curtain-anim', 'curtain-out');
+  } else {
+    curtain.classList.replace('curtain-out', 'curtain-anim');
+  }
+}
+
+menuBars.addEventListener('click', HamburgerMenu);
+navList.forEach((link) => {
+  link.addEventListener('click', HamburgerMenu);
+});
